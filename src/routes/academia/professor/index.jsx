b16aec2550764e5206a8e18659c8f678d8669496
Prince Hymn <ngiruprince@gmail.com>
2026-05-26 20:09:21 +0200

    Professor Dashboard - Perfomance

--- src/routes/academia/professor/index.jsx
+++ src/routes/academia/professor/index.jsx
@@ -4,6 +4,7 @@ import PrepareCourse from '../../../pages/academia/professor/PrepareCourse';
 import Assignments from '../../../pages/academia/professor/Assignments';
 import Projects from '../../../pages/academia/professor/Projects';
 import ViewProject from '../../../pages/academia/professor/ViewProject';
+import Performance from '../../../pages/academia/professor/Performance';
 
 function AcademiaProfessorRoutes() {
   // Provide a top-level route so DashboardHome renders without the AcademiaLayout wrapper.
@@ -15,6 +16,7 @@ function AcademiaProfessorRoutes() {
       <Route path="academia/professor/assignments" element={<Assignments />} />
       <Route path="academia/professor/projects" element={<Projects />} />
       <Route path="academia/professor/view-project" element={<ViewProject />} />
+      <Route path="academia/professor/performance" element={<Performance />} />
     </>
   );
 }
