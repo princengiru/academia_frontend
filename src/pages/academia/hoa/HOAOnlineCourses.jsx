c7cb66b5dc88ec51e2bf942f5413e351dc806595
Prince Hymn <ngiruprince@gmail.com>
2026-06-15 16:21:00 +0200

    HOA Dashboard - online courses

--- src/pages/academia/hoa/HOAOnlineCourses.jsx
+++ src/pages/academia/hoa/HOAOnlineCourses.jsx
@@ -31,8 +31,7 @@ const IconUserBust = () => (
 
 const IconRightArrow = () => (
     <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <line x1="5" y1="12" x2="19" y2="12"></line>
-        <polyline points="12 5 19 12 12 19"></polyline>
+        <polyline points="9 18 15 12 9 6"></polyline>
     </svg>
 );
 
