02a148cc04cc9b1f98f3a922b0aec2c231c9b069
Prince Hymn <ngiruprince@gmail.com>
2026-07-01 00:03:12 +0200

    HOA Dashboard - Terms & Conditions

--- src/pages/academia/hoa/HOATermsConditions.jsx
+++ src/pages/academia/hoa/HOATermsConditions.jsx
@@ -25,7 +25,7 @@ const IconPencil = () => (
 );
 
 const IconChevronRight = () => (
-    <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round">
         <polyline points="9 18 15 12 9 6"></polyline>
     </svg>
 );
