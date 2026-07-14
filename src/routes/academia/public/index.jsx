c6ef95b9949ccad86563ab5a7fe16c7184612a80
fabricetfab <fabricetfab@gmail.com>
2026-07-14 12:01:33 +0200

    update public routes for projects certificates and info pages

--- src/routes/academia/public/index.jsx
+++ src/routes/academia/public/index.jsx
@@ -1,34 +1,83 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import { Navigate, Route, useLocation, useSearchParams } from 'react-router-dom';
+import { PublicLoading } from '../../../pages/academia/public/PublicPageState';
 import AcademiaIndex from '../../../pages/academia/public';
-import AcademiaJournals from '../../../pages/academia/public/journals';
+import AcademiaProjects from '../../../pages/academia/public/projects';
 import AcademiaSyllabuses from '../../../pages/academia/public/syllabuses';
 import AcademiaSyllabusPart from '../../../pages/academia/public/syllabus-part';
-import AcademiaReadContents from '../../../pages/academia/public/read-contents';
-import AcademiaReadJournal from '../../../pages/academia/public/read-journal';
+import AcademiaReadProject from '../../../pages/academia/public/read-project';
 import AcademiaReadStory from '../../../pages/academia/public/read-story';
-import AcademiaWatch from '../../../pages/academia/public/watch';
 import AcademiaAuthor from '../../../pages/academia/public/author';
-import AcademiaRewards from '../../../pages/academia/public/rewards';
+import AcademiaCertificates from '../../../pages/academia/public/certificates';
 import AcademiaCourseDetails from '../../../pages/academia/public/course-details';
 import AcademiaCourses from '../../../pages/academia/public/courses';
+import AcademiaTerms from '../../../pages/academia/public/terms';
+import AcademiaPrivacy from '../../../pages/academia/public/privacy';
+import AcademiaAdPolicy from '../../../pages/academia/public/ad-policy';
+import AcademiaHelp from '../../../pages/academia/public/help';
+import AcademiaSitemap from '../../../pages/academia/public/sitemap';
+import AcademiaNotFound from '../../../pages/academia/public/NotFoundPage';
+
+const AcademiaReadContents = lazy(() => import('../../../pages/academia/public/read-contents'));
+const AcademiaWatch = lazy(() => import('../../../pages/academia/public/watch'));
+
+function LazyReadContents() {
+  return (
+    <Suspense fallback={<PublicLoading message="Loading reader…" />}>
+      <AcademiaReadContents />
+    </Suspense>
+  );
+}
+
+function LazyWatch() {
+  return (
+    <Suspense fallback={<PublicLoading message="Loading community feed…" />}>
+      <AcademiaWatch />
+    </Suspense>
+  );
+}
+
+function RedirectJournalsToProjects() {
+  const location = useLocation();
+  return <Navigate to={`/academia/projects${location.search || ''}`} replace />;
+}
+
+function RedirectReadJournalToProject() {
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get('id');
+  return (
+    <Navigate
+      to={id ? `/academia/read-project?id=${encodeURIComponent(id)}` : '/academia/projects'}
+      replace
+    />
+  );
+}
 
 export default function AcademiaPublicRoutes() {
   return (
     <>
       <Route index element={<AcademiaIndex />} />
       <Route path="index" element={<AcademiaIndex />} />
-      <Route path="journals" element={<AcademiaJournals />} />
+      <Route path="projects" element={<AcademiaProjects />} />
+      <Route path="journals" element={<RedirectJournalsToProjects />} />
       <Route path="syllabuses" element={<AcademiaSyllabuses />} />
       <Route path="syllabus-part" element={<AcademiaSyllabusPart />} />
-      <Route path="read-contents" element={<AcademiaReadContents />} />
-      <Route path="read-journal" element={<AcademiaReadJournal />} />
+      <Route path="read-contents" element={<LazyReadContents />} />
+      <Route path="read-project" element={<AcademiaReadProject />} />
+      <Route path="read-journal" element={<RedirectReadJournalToProject />} />
       <Route path="read-story" element={<AcademiaReadStory />} />
-      <Route path="watch" element={<AcademiaWatch />} />
+      <Route path="watch" element={<LazyWatch />} />
       <Route path="author" element={<AcademiaAuthor />} />
-      <Route path="rewards" element={<AcademiaRewards />} />
+      <Route path="certificates" element={<AcademiaCertificates />} />
+      <Route path="rewards" element={<Navigate to="/academia/certificates" replace />} />
       <Route path="course-details" element={<AcademiaCourseDetails />} />
       <Route path="courses" element={<AcademiaCourses />} />
+      <Route path="terms" element={<AcademiaTerms />} />
+      <Route path="privacy" element={<AcademiaPrivacy />} />
+      <Route path="ad-policy" element={<AcademiaAdPolicy />} />
+      <Route path="help" element={<AcademiaHelp />} />
+      <Route path="sitemap" element={<AcademiaSitemap />} />
+      <Route path="*" element={<AcademiaNotFound />} />
     </>
   );
 }
